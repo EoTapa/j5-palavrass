e3cb7c3a4757681dda20a76ae2504796c8078b3e
EoTapa <00001099989504sp@al.educacao.sp.gov.br>
2024-06-11 10:46:06 -0300

    adicionando arquivos js e css

---
+++ sketh.js
@@ -0,0 +1,42 @@
+let palavra; 
+
+function setup() {
+  createCanvas(400, 400);
+  
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+  
+  let palavras = ["caminhante", "caminho","caminha"];
+
+return random(palavras);
+}
+
+function inicializaCore(){
+  background("white")
+fill ("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo){
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw(){
+  inicializaCore();
+let texto = palavraParcial(0, width);
+  text(texto, 200, 200);
+}
+
+function modoNortuno(horario) {
+  if (horario>18){
+    console.log("você precisa ligar o modo escuro!");
+  } else {
+    console.log("modo nortuno não é necessario neste momento.");
+  }
+}
+
